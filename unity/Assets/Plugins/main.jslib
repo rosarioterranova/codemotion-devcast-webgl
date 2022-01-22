f16f30d4807cf2c340d3b1903ce9b3c9f3ecdd6d
Rosario Terranova <rosarioterranova91@gmail.com>
2022-01-22 17:57:52 +0100

    added c# to js

---
+++ unity/Assets/Plugins/main.jslib
@@ -0,0 +1,6 @@
+mergeInto(LibraryManager.library, {
+
+Hello: function() {
+        window.alert("Hello, world!");
+    },
+});
--- unity/Assets/rotate.cs
+++ unity/Assets/rotate.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
+enum Direction { right, left, up, down }
 
 public class rotate : MonoBehaviour
 {
     [SerializeField]
     private float speed = 50f;
+    [SerializeField]
+    Direction direction = Direction.left;
 
-    // Start is called before the first frame update
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
     void Start()
     {
-        
+        Hello();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        Vector3 vector = Vector3.zero;
+        switch (direction)
+        {
+            case Direction.left:
+                vector = Vector3.up;
+                break;
+            case Direction.right:
+                vector = Vector3.down;
+                break;
+            case Direction.up:
+                vector = Vector3.left;
+                break;
+            case Direction.down:
+                vector = Vector3.right;
+                break;
+        }
+        transform.Rotate(vector * Time.deltaTime * speed);
     }
 }
